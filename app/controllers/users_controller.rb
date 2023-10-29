242687b00592b214507f1967871e29bb4f362238
yuzuki00 <144969224+yuzuki00@users.noreply.github.com>
2023-10-29 06:48:36 +0000

    [Finish] /users/id/showのひな型作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @users = User.all
+    @book = Book.new
     @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 end
